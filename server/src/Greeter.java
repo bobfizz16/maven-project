bf2986809df83a2a0ee95fdba133eba27426ce4a
bobfizz16 <bfinizio@comcast.net>
2018-10-16 10:39:58 -0400

    Add files via upload

---
+++ server/src/Greeter.java
@@ -0,0 +1,28 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+<<<<<<< HEAD
+  
+/**
+ * @param someone the name of a person
+ * @return greeting string
+ */
+  public final String greet(final String someone) {
+=======
+
+  //TODO: Add javadoc comment
+  public String greet(final String someone) {
+>>>>>>> 36d21564b5d65ad2b5eb3e7c41c445b6ac6c4d67
+    return String.format("Hello, %s!", someone);
+  }
+}
